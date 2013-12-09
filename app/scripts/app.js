56436cf8b1e8e0b3dc5800e192eb3065ee2c0dcf
user <roberfrias115@gmail.com>
2013-12-09 07:13:18 -0500

    buttons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,23 @@
 define([], function () {
     'use strict';
     
-    $(document).ready(function () {
-		alert("Bleh")
-	})
+    $(document).ready(function() {
+    $(".button").bind('click', $.proxy(function() {
+        var status = $(this).attr('id');
+        console.log(ThisId)
+        if ( thisId === "red") {
+        alert(status);
+	}else if ( thisId === "blue"){
+	
+	}else if ( thisId === "yellow"){
+	
+	}else if ( thisId === "green"){
+		
+	} else {
+		console.log("Fail!")
+	}	
+    
+});​
+    
     return 1;
 });
